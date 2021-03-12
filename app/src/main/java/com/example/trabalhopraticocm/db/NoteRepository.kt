9c554d19e45ccd78bcf1fdffc0b0113b72681878
desenvolvimento-wepickup <desenvolvimento@wepickup.pt>
2021-03-12 19:34:42 +0000

    create note repository

--- app/src/main/java/com/example/trabalhopraticocm/db/NoteRepository.kt
+++ app/src/main/java/com/example/trabalhopraticocm/db/NoteRepository.kt
@@ -1,4 +1,18 @@
 package com.example.trabalhopraticocm.db
 
-class NoteRepository {
+import androidx.annotation.WorkerThread
+import com.example.trabalhopraticocm.dao.NoteDao
+import com.example.trabalhopraticocm.entities.Note
+import kotlinx.coroutines.flow.Flow
+
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes: Flow<List<Note>> = noteDao.getAlphabetizedNotes()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
 }
